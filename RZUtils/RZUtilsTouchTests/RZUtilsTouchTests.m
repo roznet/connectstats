2f5eea98b243cc5aab12e2313f6906b4d2ffd97d
brice <briceguard-git@yahoo.com>
2018-01-14 12:29:23 +0000

    clean up icons for test app and start xctest link

--- RZUtils/RZUtilsTouchTests/RZUtilsTouchTests.m
+++ RZUtils/RZUtilsTouchTests/RZUtilsTouchTests.m
@@ -25,12 +25,6 @@
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-    
-}
-
 -(void)testTableRemap{
     RZTableIndexRemap * remap = [RZTableIndexRemap tableIndexRemap];
     [remap addSection:4 withRows:@[ @(9), @(0) ]];
