7a91510046691436bfe54ee5e95b0635b623a62a
brice <briceguard-git@yahoo.com>
2018-03-25 13:45:59 +0100

    Fix compile with ARC

--- ConnectStats/src/GCActivityType.m
+++ ConnectStats/src/GCActivityType.m
@@ -51,12 +51,12 @@ static GCActivityTypes * _activityTypesCache = nil;
 
 +(void)setActivityTypes:(GCActivityTypes *)a{
     if( _activityTypesCache != nil){
-        [_activityTypesCache release];
+        RZRelease(_activityTypesCache release);
         _activityTypesCache = nil;
     }
     
     _activityTypesCache = a;
-    [_activityTypesCache retain];
+    RZRetain(_activityTypesCache retain);
 }
 
 +(GCFieldCache*)fieldCache{
