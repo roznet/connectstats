098650c777f1cebb3a1f0a92dbed755d08360ecd
brice <briceguard-git@yahoo.com>
2018-12-30 15:45:25 +0100

    compiles and opens file

--- RZFitFile/src/RZFitMessage.swift
+++ RZFitFile/src/RZFitMessage.swift
@@ -11,20 +11,27 @@ import Foundation
 
 class RZFitMessage {
     
-    let num : FIT_MESG_NUM
-    let values : [String:Double]
-    let enums : [String:String]
+    let messageType : RZFitMessageType
+    private let values : [RZFitFieldKey:Double]
+    private let enums : [RZFitFieldKey:String]
     
-    init(mesg_num  : FIT_MESG_NUM, mesg_values : [String:Double], mesg_enums : [String:String]) {
-        num = mesg_num
+    var messageTypeDescription : String?{
+        return rzfit_mesg_num_string(input: messageType)
+    }
+
+    private var cacheInterpretation : [RZFitFieldKey:RZFitFieldValue]
+    
+    init(mesg_num  : FIT_MESG_NUM, mesg_values : [RZFitFieldKey:Double], mesg_enums : [RZFitFieldKey:String]) {
+        messageType = mesg_num
         values = mesg_values
         enums = mesg_enums
+        cacheInterpretation = [:]
         
     }
     
-    convenience init(mesg_num : FIT_MESG_NUM, withFitFields:[String:RZFitField]) {
-        var ivalues : [String:Double] = [:]
-        var ienums  : [String:String] = [:]
+    convenience init(mesg_num : FIT_MESG_NUM, withFitFields:[RZFitFieldKey:RZFitFieldValue]) {
+        var ivalues : [RZFitFieldKey:Double] = [:]
+        var ienums  : [RZFitFieldKey:String] = [:]
         
         for (key,field) in withFitFields {
             if let coord = field.coordinate{
@@ -44,23 +51,37 @@ class RZFitMessage {
         self.init(mesg_num: mesg_num, mesg_values: ivalues, mesg_enums: ienums)
     }
     
-    
-    func name() -> String?{
-        return rzfit_mesg_num_string(input: num)
+    func interpretedFieldKeys() -> [RZFitFieldKey] {
+        let interp = self.interpretedFields()
+        var rv : [RZFitFieldKey] = []
+        
+        for (key,_) in interp {
+            rv.append(key)
+        }
+        return rv
     }
     
-    func interpretFields() -> [String:RZFitField] {
-        var rv :[String:RZFitField] = [:]
+    func interpretedField(key:RZFitFieldKey) -> RZFitFieldValue? {
+        var interp = self.interpretedFields()
+        
+        return interp[key]
+    }
+    
+    func interpretedFields() -> [RZFitFieldKey:RZFitFieldValue] {
+        if self.cacheInterpretation.count > 0 {
+            return self.cacheInterpretation
+        }
+        
+        var rv :[String:RZFitFieldValue] = [:]
         
         for (key,val) in values {
             if( key.hasSuffix("_lat") ) {
-    
                 let lon = key.replacingOccurrences(of: "_lat", with: "_long")
                 let newkey = key.replacingOccurrences(of: "_lat", with:"")
                 let latitude = val * 180.0/2147483648.0 // SEMICIRCLE_TO_DEGREE
                 if var longitude = values[lon] {
                     longitude = longitude * 180.0/2147483648.0 // SEMICIRCLE_TO_DEGREE
-                    rv[ newkey ] = RZFitField(latitude: latitude, longitude: longitude)
+                    rv[ newkey ] = RZFitFieldValue(latitude: latitude, longitude: longitude)
                 }
             }else if( key.hasSuffix( "_long") ){
                 // handled by _lat
@@ -69,38 +90,39 @@ class RZFitMessage {
             else if( key == "timestamp" || key == "start_time" || key == "local_timestamp"){
                 // Fit file are in seconds since UTC 00:00 Dec 31 1989 = -347241600
                 let date = Date(timeIntervalSinceReferenceDate: -347241600+val)
-                rv[ key ] = RZFitField(withTime: date )
+                rv[key] = RZFitFieldValue(withTime: date )
             }
             else if let unit = rzfit_unit_for_field(field: key) {
-                rv[ key ]  = RZFitField(withValue: val, andUnit: unit)
+                rv[key] =  RZFitFieldValue(withValue: val, andUnit: unit)
             }else if( key == "product" ){
                 let product_int : FIT_UINT16 = FIT_UINT16(val)
                 if let mapped = rzfit_garmin_product_string(input: product_int ) {
-                    rv[ key ] = RZFitField(withName:  mapped)
+                    rv[key] = RZFitFieldValue(withName:  mapped)
                 }else{
-                    rv[ key ] = RZFitField(withName: "\(product_int)")
+                    rv[key] = RZFitFieldValue(withName: "\(product_int)")
                 }
             }else if( key == "device_type" ){
                 let device_type_int = FIT_UINT8(val)
                 if let mapped = rzfit_antplus_device_type_string(input: device_type_int) {
-                    rv[key] = RZFitField(withName: mapped)
+                    rv[key] = RZFitFieldValue(withName: mapped)
                 }else{
-                    rv[key] = RZFitField(withName: "\(device_type_int)")
+                    rv[key] = RZFitFieldValue(withName: "\(device_type_int)")
                 }
             }else{
-                rv[ key ] = RZFitField(withValue: val )
+                rv[key] = RZFitFieldValue(withValue: val )
             }
         }
         
         for (key,val) in enums {
-            rv[ key ] = RZFitField(withName: val )
+            rv[ key ] = RZFitFieldValue(withName: val )
         }
-        
-        return rv
+        self.cacheInterpretation = rv
+        return self.cacheInterpretation
     }
+    
     func description() -> String {
         var rv = [ "FitMessage(" ]
-        if let name = self.name() {
+        if let name = self.messageTypeDescription {
             rv.append(name)
         }else{
             rv.append("Unknown")
@@ -111,7 +133,7 @@ class RZFitMessage {
         return rv.joined(separator: "")
     }
     
-    func units() -> [String:String] {
+    func units() -> [RZFitFieldKey:String] {
         var rv : [String:String] = [:]
         
         for field in values.keys {
