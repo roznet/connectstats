744c39792e348fa4829577d0641d33144e0178bf
brice <briceguard-git@yahoo.com>
2019-08-18 19:19:43 +0100

    Upgraded to python3 and rerun colors icons

--- ConnectStats/credentials.py
+++ ConnectStats/credentials.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 #
 # Simple utility to generate a credentials config file as we update the main credentials.json
 #
@@ -10,9 +10,9 @@ import json
 with open( 'credentials.json', 'r') as jsonfile:
     full = json.load( jsonfile );
     sample = dict()
-    for service,one in full.iteritems():
+    for service,one in full.items():
         servicedict = dict()
-        for key,val in one.iteritems():
+        for key,val in one.items():
             if key.endswith('_url' ) or key.endswith('_callback') or key.endswith('_param'):
                 servicedict[key] = val
             else:
--- ConnectStats/icons/makecolortheme.py
+++ ConnectStats/icons/makecolortheme.py
@@ -1,14 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import sys
 import os
 import json
+import argparse
 
-theme = "default"
-doit = True
-
-def uicolorhex( hex, alp ):
-    return {"red":"0x{}".format(hex[2:4]), "green":"0x{}".format(hex[4:6]),"blue":"0x{}".format(hex[6:8]),"alpha":"{}".format( alp ) }
 
 
 GC_TYPE_RUNNING =     "running";
@@ -34,41 +30,66 @@ GC_TYPE_ROWING =      "rowing";
 
 GC_TYPE_UNCATEGORIZED = "uncategorized";
 
-defs = {
-    "{theme}-SwimStrokeColor-gcSwimStrokeFree": {"red":"0xC4", "green":"0x3D", "blue":"0xBF", "alpha":"0.8"},
-    "{theme}-SwimStrokeColor-gcSwimStrokeBack": {"red":"0x1F", "green":"0x8E","blue":"0xF0", "alpha":"0.8"},
-    "{theme}-SwimStrokeColor-gcSwimStrokeBreast": {"red":"0x95", "green":"0xDE", "blue":"0x2B", "alpha":"0.8"},
-    "{theme}-SwimStrokeColor-gcSwimStrokeButterfly": {"red":"0xD5", "green":"0x76","blue":"0xD1", "alpha":"0.8"},
-    "{theme}-SwimStrokeColor-gcSwimStrokeOther": {"red":"0x61", "green":"0xAF","blue":"0xF3", "alpha":"0.8"},
-    "{theme}-SwimStrokeColor-gcSwimStrokeMixed": {"red":"0x61", "green":"0xAF", "blue":"0xF3", "alpha":"0.8"},
+gcGCSkinKeyCalendarElementWeekdayTextColor = 0
+gcGCSkinKeyCalendarElementDayCurrentMonthTextColor = 1
+gcGCSkinKeyCalendarElementDayAdjacentMonthTextColor = 2
+gcGCSkinKeyCalendarElementDaySelectedTextColor = 3
+gcGCSkinKeyCalendarElementSeparatorColor = 4
+gcGCSkinKeyCalendarElementTileColor = 5
+gcGCSkinKeyCalendarElementTileSelectedColor = 6
+gcGCSkinKeyCalendarElementTileTodayColor = 7
+gcGCSkinKeyCalendarElementTileTodaySelectedColor = 8
+
+
+def uicolorhex( hex, alp ):
+    return {"red":"0x{}".format(hex[2:4]), "green":"0x{}".format(hex[4:6]),"blue":"0x{}".format(hex[6:8]),"alpha":"{}".format( alp ) }
+
+def build_defs(theme):
+    defs = {
+        "{theme}-SwimStrokeColor-gcSwimStrokeFree": {"red":"0xC4", "green":"0x3D", "blue":"0xBF", "alpha":"0.8"},
+        "{theme}-SwimStrokeColor-gcSwimStrokeBack": {"red":"0x1F", "green":"0x8E","blue":"0xF0", "alpha":"0.8"},
+        "{theme}-SwimStrokeColor-gcSwimStrokeBreast": {"red":"0x95", "green":"0xDE", "blue":"0x2B", "alpha":"0.8"},
+        "{theme}-SwimStrokeColor-gcSwimStrokeButterfly": {"red":"0xD5", "green":"0x76","blue":"0xD1", "alpha":"0.8"},
+        "{theme}-SwimStrokeColor-gcSwimStrokeOther": {"red":"0x61", "green":"0xAF","blue":"0xF3", "alpha":"0.8"},
+        "{theme}-SwimStrokeColor-gcSwimStrokeMixed": {"red":"0x61", "green":"0xAF", "blue":"0xF3", "alpha":"0.8"},
 
     "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_SWIMMING,theme=theme): uicolorhex("0xFFE4A9", 1.0),
-    "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_CYCLING,theme=theme):  uicolorhex("0xFFDADA", 1.0),
-    "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_RUNNING,theme=theme):  uicolorhex("0xDCEEFF", 1.0),
-    "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_HIKING,theme=theme):   uicolorhex("0xE8C89E", 1.0),
-    "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_FITNESS,theme=theme):  uicolorhex("0xCAA4E8", 1.0),
-    "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_TENNIS,theme=theme):   uicolorhex("0x22B5B0", 1.0),
-    "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_MULTISPORT,theme=theme):uicolorhex("0xA6BB82", 1.0),
-    "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_OTHER,theme=theme):uicolorhex("0xD2D2D2", 1.0),
-    "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_SKI_BACK,theme=theme): uicolorhex("0xa2d7b5", 1.0),
-    "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_SKI_DOWN,theme=theme): uicolorhex("0xecf0f1",1.0),
+        "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_CYCLING,theme=theme):  uicolorhex("0xFFDADA", 1.0),
+        "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_RUNNING,theme=theme):  uicolorhex("0xDCEEFF", 1.0),
+        "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_HIKING,theme=theme):   uicolorhex("0xE8C89E", 1.0),
+        "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_FITNESS,theme=theme):  uicolorhex("0xCAA4E8", 1.0),
+        "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_TENNIS,theme=theme):   uicolorhex("0x22B5B0", 1.0),
+        "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_MULTISPORT,theme=theme):uicolorhex("0xA6BB82", 1.0),
+        "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_OTHER,theme=theme):uicolorhex("0xD2D2D2", 1.0),
+        "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_SKI_BACK,theme=theme): uicolorhex("0xa2d7b5", 1.0),
+        "{theme}-ActivityCellLighterBackgroundColor-{}".format(GC_TYPE_SKI_DOWN,theme=theme): uicolorhex("0xecf0f1",1.0),
 
     "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_SWIMMING,theme=theme): uicolorhex("0xFFD466", 1.0),
-    "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_CYCLING,theme=theme):  uicolorhex("0xFFA0A0", 1.0),
-    "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_RUNNING,theme=theme):  uicolorhex("0x98D3FF", 1.0),
-    "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_HIKING,theme=theme):   uicolorhex("0xC8A26A", 1.0),
-    "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_FITNESS,theme=theme):  uicolorhex("0xF169EF", 1.0),
-    "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_TENNIS,theme=theme):   uicolorhex("0x96CC00", 1.0),
-    "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_MULTISPORT,theme=theme):uicolorhex("0xA6BB82", 1.0),
-    "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_OTHER,theme=theme):uicolorhex("0xA6BB82", 1.0),
-    "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_SKI_BACK,theme=theme): uicolorhex("0xa2d7b5", 1.0),
-    "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_SKI_DOWN,theme=theme): uicolorhex("0xbdc3c7", 1.0)
-}
+        "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_CYCLING,theme=theme):  uicolorhex("0xFFA0A0", 1.0),
+        "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_RUNNING,theme=theme):  uicolorhex("0x98D3FF", 1.0),
+        "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_HIKING,theme=theme):   uicolorhex("0xC8A26A", 1.0),
+        "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_FITNESS,theme=theme):  uicolorhex("0xF169EF", 1.0),
+        "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_TENNIS,theme=theme):   uicolorhex("0x96CC00", 1.0),
+        "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_MULTISPORT,theme=theme):uicolorhex("0xA6BB82", 1.0),
+        "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_OTHER,theme=theme):uicolorhex("0xA6BB82", 1.0),
+        "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_SKI_BACK,theme=theme): uicolorhex("0xa2d7b5", 1.0),
+        "{theme}-ActivityCellDarkerBackgroundColor-{}".format(GC_TYPE_SKI_DOWN,theme=theme): uicolorhex("0xbdc3c7", 1.0),
 
+    "{theme}-CalendarColors-{}".format(gcGCSkinKeyCalendarElementWeekdayTextColor,theme=theme):          {"red":0.3, "green":0.3, "blue":0.3, "alpha":1.0},
+        "{theme}-CalendarColors-{}".format(gcGCSkinKeyCalendarElementDayCurrentMonthTextColor,theme=theme):  uicolorhex("0x000000", 1.0),
+        "{theme}-CalendarColors-{}".format(gcGCSkinKeyCalendarElementDayAdjacentMonthTextColor,theme=theme): uicolorhex("0xA9A9A9", 1.0),
+        "{theme}-CalendarColors-{}".format(gcGCSkinKeyCalendarElementDaySelectedTextColor,theme=theme):      uicolorhex("0xFFFFFF", 1.0),
+        "{theme}-CalendarColors-{}".format(gcGCSkinKeyCalendarElementSeparatorColor,theme=theme): {"red":0.63, "green":0.65, "blue":0.68, "alpha":1.0},
+        "{theme}-CalendarColors-{}".format(gcGCSkinKeyCalendarElementTileColor,theme=theme):                 uicolorhex("0xEBEBEB", 1.0),
+        "{theme}-CalendarColors-{}".format(gcGCSkinKeyCalendarElementTileSelectedColor,theme=theme):         uicolorhex("0x1843c7", 1.0),
+        "{theme}-CalendarColors-{}".format(gcGCSkinKeyCalendarElementTileTodayColor,theme=theme):            uicolorhex("0x7788a2", 1.0),
+        "{theme}-CalendarColors-{}".format(gcGCSkinKeyCalendarElementTileTodaySelectedColor,theme=theme):    uicolorhex("0x3b7dde", 1.0),
+    }
+    return defs
 
 def mkdir(path,doit):
     if not os.path.isdir( path ):
-        print 'mkdir {}'.format( path )
+        print( 'mkdir {}'.format( path ) )
         if doit:
             os.mkdir(path)
 
@@ -96,20 +117,32 @@ def colorset_json( defs ):
     return rv
     
             
+def save_theme(theme, doit = False):
 
-assetdir = 'Colors-{}.xcassets'.format( theme )
-if not os.path.isdir( assetdir ):
-    mkdir( assetdir, doit )
-save_json( "{}/Contents.json".format( assetdir ), info_json(), doit )
+    defs = build_defs( theme )
+    
+    assetdir = 'Colors-{}.xcassets'.format( theme )
+    if not os.path.isdir( assetdir ):
+        mkdir( assetdir, doit )
+    save_json( "{}/Contents.json".format( assetdir ), info_json(), doit )
 
-for (key,components) in defs.iteritems():
-    name = key.format( theme = theme )
+    for (key,components) in defs.items():
+        name = key.format( theme = theme )
 
-    colorsetdir = '{}/{}.colorset'.format( assetdir, name )
-    mkdir( colorsetdir, doit )
+        colorsetdir = '{}/{}.colorset'.format( assetdir, name )
+        mkdir( colorsetdir, doit )
 
-    contents = colorset_json( components )
+        contents = colorset_json( components )
 
-    jsonpath = '{}/Contents.json'.format( colorsetdir )
-    save_json( jsonpath, contents, doit )
-           
+        jsonpath = '{}/Contents.json'.format( colorsetdir )
+        save_json( jsonpath, contents, doit )
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser( description='Build Theme Base', formatter_class=argparse.RawTextHelpFormatter )
+    parser.add_argument( 'theme' )
+    parser.add_argument( '-s', '--save', help='save directory', action='store_true' )
+    parser.add_argument( '-v', '--verbose', help='verbose output', action='store_true' )
+    args = parser.parse_args()
+
+    save_theme( args.theme, args.save )
