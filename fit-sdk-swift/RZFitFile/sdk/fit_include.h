9eedcdc8e6cd99c5e1549c0cac62bc514043c238
brice <briceguard-git@yahoo.com>
2020-02-08 17:57:05 +0000

    Upgraded FitSDK 20.22

--- fit-sdk-swift/RZFitFile/sdk/fit_include.h
+++ fit-sdk-swift/RZFitFile/sdk/fit_include.h
@@ -7,7 +7,7 @@
 // warranties of merchantability, non-infringement, or fitness for a particular
 // purpose, are specifically disclaimed.
 //
-// Copyright 2019 Garmin Canada Inc.
+// Copyright 2020 Garmin Canada Inc.
 ////////////////////////////////////////////////////////////////////////////////
 
 
