9bd6f09bb26646f8b1892a42b99b0abaafdfb1d1
brice <briceguard-git@yahoo.com>
2020-04-18 17:04:51 +0100

    disable new feature for release 5.6

--- ConnectStats/src/GCStatsMultiFieldViewControllerConsts.h
+++ ConnectStats/src/GCStatsMultiFieldViewControllerConsts.h
@@ -32,8 +32,8 @@
 #define GC_SUMMARY_CUMULATIVE_DISTANCE  0
 #define GC_SUMMARY_PERFORMANCE          1
 #define GC_SUMMARY_DERIVED              2
-#define GC_SUMMARY_DERIVED_HIST         3
-#define GC_SUMMARY_END                  4
+#define GC_SUMMARY_END                  3
 // Disabled
+#define GC_SUMMARY_DERIVED_HIST         4
 
 
