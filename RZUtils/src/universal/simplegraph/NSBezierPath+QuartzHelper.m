765c668e00a07d99845cff31435c9645d9aa8b05
brice <briceguard-git@yahoo.com>
2020-05-21 20:47:06 +0100

    remove unused case triggering warning

--- RZUtils/src/universal/simplegraph/NSBezierPath+QuartzHelper.m
+++ RZUtils/src/universal/simplegraph/NSBezierPath+QuartzHelper.m
@@ -39,96 +39,10 @@ NSString * NSStringFromCGPoint(CGPoint point){
     return [NSString stringWithFormat:@"(%f,%f)", point.x, point.y];
 }
 
-static void CGPathCallback(void *info, const CGPathElement *element)
-{
-    NSBezierPath *path = (__bridge NSBezierPath *)(info);
-    CGPoint *points = element->points;
-
-    switch (element->type) {
-        case kCGPathElementMoveToPoint:
-        {
-            [path moveToPoint:NSMakePoint(points[0].x, points[0].y)];
-            break;
-        }
-        case kCGPathElementAddLineToPoint:
-        {
-            [path lineToPoint:NSMakePoint(points[0].x, points[0].y)];
-            break;
-        }
-        case kCGPathElementAddQuadCurveToPoint:
-        {
-            NSPoint currentPoint = [path currentPoint];
-            NSPoint interpolatedPoint = NSMakePoint((currentPoint.x + 2*points[0].x) / 3, (currentPoint.y + 2*points[0].y) / 3);
-            [path curveToPoint:NSMakePoint(points[1].x, points[1].y) controlPoint1:interpolatedPoint controlPoint2:interpolatedPoint];
-            break;
-        }
-        case kCGPathElementAddCurveToPoint:
-        {
-            [path curveToPoint:NSMakePoint(points[2].x, points[2].y) controlPoint1:NSMakePoint(points[0].x, points[0].y) controlPoint2:NSMakePoint(points[1].x, points[1].y)];
-            break;
-        }
-        case kCGPathElementCloseSubpath:
-        {
-            [path closePath];
-            break;
-        }
-    }
-}
-
 @implementation NSBezierPath (QuartzHelper)
 
 -(void)addLineToPoint:(CGPoint)point{
     [self lineToPoint:point];
-}
-
-+ (NSBezierPath *)bezierPathWithCGPath:(CGPathRef)pathRef{
-    NSBezierPath *path = [NSBezierPath bezierPath];
-    CGPathApply(pathRef, (__bridge void *)(path), CGPathCallback);
-
-    return path;
-}
-
-- (CGPathRef)CGPath
-{
-    NSUInteger i, numElements;
-
-    CGPathRef           immutablePath = NULL;
-
-    numElements = [self elementCount];
-    if (numElements > 0)
-    {
-        CGMutablePathRef    path = CGPathCreateMutable();
-        NSPoint             points[3];
-
-        for (i = 0; i < numElements; i++)
-        {
-            switch ([self elementAtIndex:i associatedPoints:points])
-            {
-                case NSMoveToBezierPathElement:
-                    CGPathMoveToPoint(path, NULL, points[0].x, points[0].y);
-                    break;
-
-                case NSLineToBezierPathElement:
-                    CGPathAddLineToPoint(path, NULL, points[0].x, points[0].y);
-                    break;
-
-                case NSCurveToBezierPathElement:
-                    CGPathAddCurveToPoint(path, NULL, points[0].x, points[0].y,
-                                          points[1].x, points[1].y,
-                                          points[2].x, points[2].y);
-                    break;
-
-                case NSClosePathBezierPathElement:
-                    CGPathCloseSubpath(path);
-                    break;
-            }
-        }
-
-        immutablePath = CGPathCreateCopy(path);
-        CGPathRelease(path);
-    }
-
-    return immutablePath;
 }
 
 @end
