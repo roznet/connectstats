92e84c2221e50f8030d52615ac7415e5d00199c4
brice <briceguard-git@yahoo.com>
2020-10-19 09:15:05 +0100

    temp file save

---
+++ RZUtils/src/universal/stats/GCStatsCluster.h
@@ -0,0 +1,17 @@
+//
+//  GCStatsCluster.h
+//  RZUtils
+//
+//  Created by Brice Rosenzweig on 18/10/2020.
+//  Copyright © 2020 Brice Rosenzweig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GCStatsCluster : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RZUtils/src/universal/stats/GCStatsCluster.m
@@ -0,0 +1,37 @@
+//
+//  GCStatsCluster.m
+//  RZUtils
+//
+//  Created by Brice Rosenzweig on 18/10/2020.
+//  Copyright © 2020 Brice Rosenzweig. All rights reserved.
+//
+
+#import "GCStatsCluster.h"
+
+@implementation GCStatsCluster
+
+-(void)kmean{
+    /*
+    size_t n; // number of points
+    size_t d; // dimension of point (2d -> 2, etc)
+    size_t k; // number of cluster
+
+    double points[d*n]; // points
+    double cluster_allocation[k*n]; // 0 or 1
+    double cluster_center[k*d*n];
+    
+    //cluster centers: c_(n*d)
+
+
+    //for( k in 0..n)
+      //dist_k = (p_d - c_k)' * (p_d - c_k);
+      //  c = k if dist_k < curr_k;
+    //curr_k = dist_k;
+      
+*/
+
+
+
+}
+
+@end
