34dc385a7491523cf7dad68d1c03fc5b443c4500
brice <briceguard-git@yahoo.com>
2020-10-22 08:33:58 +0100

    upgrade xcode 12.1

--- RZUtils/src/universal/stats/GCStatsCluster.m
+++ RZUtils/src/universal/stats/GCStatsCluster.m
@@ -12,25 +12,27 @@
 
 -(void)kmean{
     /*
-    size_t n; // number of points
-    size_t d; // dimension of point (2d -> 2, etc)
-    size_t k; // number of cluster
+    size_t n_points; // number of points
+    size_t n_dimensions; // dimension of point (2d -> 2, etc)
+    size_t n_clusters; // number of cluster
 
-    double points[d*n]; // points
-    double cluster_allocation[k*n]; // 0 or 1
-    double cluster_center[k*d*n];
+    double points[n_dimensions*n_points]; // points[ i_point * d + i_dimension ]
+    size_t cluster_allocation[n_points]; // cluster_allocation[ i_point * k + i_cluster ] =  0 or 1 (allocated)
+    double cluster_center[n_clusters*n_dimensions*n_points]; // cluster_center[ i_point * (k*d) + (i_cluster * d) + i_dimension ]
     
-    //cluster centers: c_(n*d)
-
+    for( size_t i_point = 0; i_point < n_points; i_point++ ){
+        // for each point:
+        
+        
+    }
 
     //for( k in 0..n)
       //dist_k = (p_d - c_k)' * (p_d - c_k);
       //  c = k if dist_k < curr_k;
     //curr_k = dist_k;
       
+
 */
-
-
 
 }
 
