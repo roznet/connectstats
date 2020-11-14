12f094b8c7748b59fba89a3fb8766000328fcb47
brice <briceguard-git@yahoo.com>
2020-11-14 14:26:03 +0000

    Keep file needed on server for applink to work

---
+++ www/app-ios/index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
+<title>ConnectStats App Links</title>
+<style type="text/css">
+body{
+	font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+	font-size: 15px;
+	font-weight: 300;
+}
+</style>
+</head>
+
+<body>
+  <div id="page">
+    <header id="branding">
+      <a href="https://ro-z.net/blog/">
+		<img src="https://ro-z.net/blog/wp-content/uploads/2015/09/cropped-BlogBannerBlue.jpg" width="1000" height="287" alt="ConnectStats Blog" />
+      </a>
+    </header>
+    <div id="content">
+      <h2>ConnectStats Application Link</h2>
+      <p>
+		This is intended to be opened from an iPhone where ConnectStats is installed.
+
+
+      </p>
+<pre>
+<?php
+        print_r($_SERVER['REQUEST_URI']);
+?>
+</pre>
+    </div>
+  </div>
+<body>
