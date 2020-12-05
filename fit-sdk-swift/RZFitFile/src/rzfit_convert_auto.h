f74ce1fbdf390983ecf3921dfd65a16c358659d4
brice <briceguard-git@yahoo.com>
2020-12-05 15:24:53 +0000

    cleanup

--- fit-sdk-swift/RZFitFile/src/rzfit_convert_auto.h
+++
@@ -1,8 +0,0 @@
-// This file is auto generated, Do not edit
-
-@import Foundation;
-#include "fit_config.h"
-#include "fit_example.h"
-#include "fit_convert.h"
-
-extern NSString * objc_rzfit_field_num_to_field( FIT_MESG_NUM messageType, FIT_UINT16 field );
