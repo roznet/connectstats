1763112ddef991df7464b595ff0f1a140fe851a6
brice <briceguard-git@yahoo.com>
2021-02-13 16:25:19 +0000

    proper order for logic of api check

--- ConnectStats/src/GCConnectStatsRequestLogin.m
+++ ConnectStats/src/GCConnectStatsRequestLogin.m
@@ -96,53 +96,55 @@ typedef NS_ENUM(NSUInteger,GCConnectStatsRequestLoginStage) {
 -(void)process:(NSData *)theData andDelegate:(id<GCWebRequestDelegate>)delegate{
     self.delegate = delegate;
     
-    if (![self isSignedIn]) {
-        dispatch_async(dispatch_get_main_queue(), ^(){
-            [self processNewStage];
-        });
-        dispatch_async(dispatch_get_main_queue(),^(){
-            [self signIn];
-        });
-        
-    }else{
-
-        if( self.loginStage == GCConnectStatsRequestLoginStageValidateUser){
+    if (self.loginStage == GCConnectStatsRequestLoginStageAPICheck){
+        if( theData != nil){
             NSData * jsonData = theData;
             NSDictionary * info = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingAllowFragments error:nil];
-            if( [info isKindOfClass:[NSDictionary class]] && [info[@"cs_user_id"] respondsToSelector:@selector(integerValue)] && [info[@"cs_user_id"] integerValue] == self.userId){
-                RZLog(RZLogInfo, @"Validated user %@", info[@"cs_user_id"]);
-            }else{
-                RZLog(RZLogWarning, @"Invalid user %@ != %@", info[@"cs_user_id"], @(self.userId));
-            }
-        }else if (self.loginStage == GCConnectStatsRequestLoginStageAPICheck){
-            if( theData != nil){
-                NSData * jsonData = theData;
-                NSDictionary * info = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingAllowFragments error:nil];
-                if( [info isKindOfClass:[NSDictionary class]] && [info[@"status"] respondsToSelector:@selector(integerValue)] && [info[@"status"] integerValue] == 1){
-                    RZLog(RZLogInfo, @"Api Check Success");
-                    
-                    if( [info[@"redirect"] isKindOfClass:[NSString class]] ){
-                        gcWebConnectStatsConfig redirect = GCWebConnectStatsConfigForRedirect(info[@"redirect"]);
-                        gcWebConnectStatsConfig currentConfig = [GCAppGlobal webConnectsStatsConfig];
-                        if( redirect != gcWebConnectStatsConfigEnd && redirect != currentConfig){
-                            RZLog( RZLogInfo, @"API Check requesting redirect from %@ to %@",
-                                  GCWebConnectStatsApiCheck(currentConfig),
-                                  GCWebConnectStatsApiCheck(redirect));
-                            
-                            [[GCAppGlobal profile] configSet:CONFIG_CONNECTSTATS_CONFIG intVal:redirect];
-                            [GCAppGlobal saveSettings];
-                        }else{
-                            RZLog(RZLogInfo, @"API Check success. Already redirected." );
-                        }
+            if( [info isKindOfClass:[NSDictionary class]] && [info[@"status"] respondsToSelector:@selector(integerValue)] && [info[@"status"] integerValue] == 1){
+                RZLog(RZLogInfo, @"Api Check Success");
+                
+                if( [info[@"redirect"] isKindOfClass:[NSString class]] ){
+                    gcWebConnectStatsConfig redirect = GCWebConnectStatsConfigForRedirect(info[@"redirect"]);
+                    gcWebConnectStatsConfig currentConfig = [GCAppGlobal webConnectsStatsConfig];
+                    if( redirect != gcWebConnectStatsConfigEnd && redirect != currentConfig){
+                        RZLog( RZLogInfo, @"API Check requesting redirect from %@ to %@",
+                              GCWebConnectStatsApiCheck(currentConfig),
+                              GCWebConnectStatsApiCheck(redirect));
+                        
+                        [[GCAppGlobal profile] configSet:CONFIG_CONNECTSTATS_CONFIG intVal:redirect];
+                        [GCAppGlobal saveSettings];
+                    }else{
+                        RZLog(RZLogInfo, @"API Check success. Already redirected." );
                     }
-                }else{
-                    RZLog(RZLogError, @"API Check FAILED %@", info);
                 }
             }else{
-                RZLog(RZLogInfo, @"API Check not supported");
+                RZLog(RZLogError, @"API Check FAILED %@", info);
             }
+        }else{
+            RZLog(RZLogInfo, @"API Check not supported");
         }
         [self processDone];
+    }else{
+        if (![self isSignedIn]) {
+            dispatch_async(dispatch_get_main_queue(), ^(){
+                [self processNewStage];
+            });
+            dispatch_async(dispatch_get_main_queue(),^(){
+                [self signIn];
+            });
+            
+        }else{
+            if( self.loginStage == GCConnectStatsRequestLoginStageValidateUser){
+                NSData * jsonData = theData;
+                NSDictionary * info = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingAllowFragments error:nil];
+                if( [info isKindOfClass:[NSDictionary class]] && [info[@"cs_user_id"] respondsToSelector:@selector(integerValue)] && [info[@"cs_user_id"] integerValue] == self.userId){
+                    RZLog(RZLogInfo, @"Validated user %@", info[@"cs_user_id"]);
+                }else{
+                    RZLog(RZLogWarning, @"Invalid user %@ != %@", info[@"cs_user_id"], @(self.userId));
+                }
+                [self processDone];
+            }
+        }
     }
 }
 
