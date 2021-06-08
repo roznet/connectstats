0bfd06a5f7a06994d08a480ca63abfb315b48698
brice <briceguard-git@yahoo.com>
2021-06-08 16:18:36 +0100

    fix to use new theme by default

--- ConnectStats/src/GCAppDelegate.m
+++ ConnectStats/src/GCAppDelegate.m
@@ -569,7 +569,7 @@ void checkVersion(void){
     BOOL needToSaveSettings = false;
     if( @available( iOS 13.0, *)){
         if( [[GCAppGlobal profile] configGetBool:@"CONFIG_FIRST_TIME_IOS13" defaultValue:true] ){
-            [[GCAppGlobal profile] configSet:CONFIG_SKIN_NAME stringVal:kGCSkinNameiOS13];
+            [[GCAppGlobal profile] configSet:CONFIG_SKIN_NAME stringVal:kGCSkinName2021];
             [[GCAppGlobal profile] configSet:@"CONFIG_FIRST_TIME_IOS13" boolVal:false];
             needToSaveSettings = true;
         }
