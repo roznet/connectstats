9ce08f789898545f50987db322c771c6cefe9f78
brice <briceguard-git@yahoo.com>
2021-11-28 14:33:53 +0100

    required service only checks password in not successful, as it could fail in backgorund when keychain not yet authorised

--- ConnectStats/src/GCConnectStatsRequest.m
+++ ConnectStats/src/GCConnectStatsRequest.m
@@ -41,8 +41,6 @@
 @property (nonatomic,assign) NSUInteger userId;
 @property (nonatomic,retain) OAuth1Controller * oauth1Controller;
 @property (nonatomic,retain) WKWebView * webView;
-#pragma message "Temporary remove before PROD"
-@property (nonatomic,assign) BOOL signinMissingFlag;
 @end
 
 @implementation GCConnectStatsRequest
@@ -140,19 +138,7 @@
         [self checkToken];
     }
     BOOL rv = self.oauthToken != nil && self.oauthTokenSecret != nil && self.userId != 0 && self.tokenId != 0;
-        
-    if( rv ){
-        if( self.signinMissingFlag){
-            self.signinMissingFlag = false;
-            RZLog(RZLogInfo, @"signedIn true: %@ %@ userId=%@ tokenId=%@", self.oauthToken, self.oauthTokenSecret, @(self.userId), @(self.tokenId));
-        }
-    }else{
-        if( !self.signinMissingFlag ){
-            self.signinMissingFlag = true;
-            RZLog(RZLogInfo, @"signedIn false: %@ %@ userId=%@ tokenId=%@", self.oauthToken, self.oauthTokenSecret, @(self.userId), @(self.tokenId));
-        }
-    }
-    
+            
     if( rv && ! self.oauth1Controller){
         [self buildOAuthController];
     }
